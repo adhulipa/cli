b2b096b86badffb38e69c0a528008dc79a5a3c4e
Daniel Nephin <dnephin@docker.com>
2018-03-06 14:44:13 -0500

    Convert to assert.NilError
    Using:
    
      git grep -l '^\s\+assert\.Check(t, err)$' | \
        xargs sed -i -e 's/^\(\s\+assert\)\.Check(t, err)$/\1.NilError(t, err)/'
    
    Signed-off-by: Daniel Nephin <dnephin@docker.com>

--- cli/command/image/prune_test.go
+++ cli/command/image/prune_test.go
@@ -99,7 +99,7 @@ func TestNewPruneCommandSuccess(t *testing.T) {
 		cmd.SetOutput(ioutil.Discard)
 		cmd.SetArgs(tc.args)
 		err := cmd.Execute()
-		assert.Check(t, err)
+		assert.NilError(t, err)
 		golden.Assert(t, cli.OutBuffer().String(), fmt.Sprintf("prune-command-success.%s.golden", tc.name))
 	}
 }
